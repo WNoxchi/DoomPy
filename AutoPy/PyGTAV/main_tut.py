af6d0c9daf9acde9d44363fae8dcfe20edf64d06
Wayne Nixalo <wayne.noxchi@gmail.com>
2017-11-04 19:59:54 -0400

    closed paren

--- AutoPy/PyGTAV/main_tut.py
+++ AutoPy/PyGTAV/main_tut.py
@@ -11,8 +11,8 @@
 # Add paths to utility folders:
 from sys import path as syspath
 from os import path as ospath; from os import getcwd
-syspath.insert(1, ospath.join(getcwd() + '1-7_code/')
-syspath.insert(1, ospath.join(getcwd() + '8-13_code/')
+syspath.insert(1, ospath.join(getcwd() + '1-7_code/'))
+syspath.insert(1, ospath.join(getcwd() + '8-13_code/'))
 
 import numpy as np
 import cv2
@@ -46,18 +46,3 @@ while True:
     screen = grab_screen(region=(0,40,800,640))
     print('Frame took {} seconds'.format(time.time()-last_time))
     last_time = time.time()
-    new_screen,original_image, m1, m2 = process_img(screen)
-    #cv2.imshow('window', new_screen)
-    cv2.imshow('window2',cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
-
-    if m1 < 0 and m2 < 0:
-        right()
-    elif m1 > 0  and m2 > 0:
-        left()
-    else:
-        straight()
-
-    #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
