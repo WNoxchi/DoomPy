c448ba296bf73ae55c5b59242ed682bba75dc93b
Wayne Nixalo <wayne.noxchi@gmail.com>
2017-11-05 18:38:43 -0500

    updated alexnet cnn

--- AutoPy/PyGTAV/PyGTAV_11_trainmodel.py
+++ AutoPy/PyGTAV/PyGTAV_11_trainmodel.py
@@ -30,7 +30,7 @@ train_labels   = [i[1] for i in train]
 test_features = np.array([i[0] for i in test]).reshape(-1, WIDTH, HEIGHT, 1)
 test_labels   = [i[1] for i in test]
 
-model.fit({'input': train_features}, {'targets:' train_labels}, n_epochs=EPOCHS,
+model.fit({'input': train_features}, {'targets': train_labels}, n_epoch=EPOCHS,
             validation_set=({'input': test_features}, {'targets:' test_labels}),
             snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
 
